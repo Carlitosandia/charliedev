a3aeb2b1b0693f6f997428062e5a4128aeae642f
Carlos Gallegos <carlosalbrtohg@gmail.com>
2024-12-18 00:19:42 -0600

    Layout div fixed

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
     return (
-        <div id='root'>
+        <div>
           <Header />
           <main>{children}</main>
           <Footer />
