a11e4b00dabe1283c2537ba182b8fcd4cb3f9bb2
Carlos Gallegos <carlosalbrtohg@gmail.com>
2024-12-17 23:58:45 -0600

    Fix vite.config.json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  build: {
+    minify: false, // Desactiva la minificación
+  },
+  base: '/', // Asegura que las rutas sean desde la raíz
 });
