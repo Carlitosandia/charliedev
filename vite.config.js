c1828625d7b60f56d51e16792c1c376bd8ff1bfb
Carlos Gallegos <carlosalbrtohg@gmail.com>
2024-12-18 00:45:29 -0600

    Modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Asegura rutas desde la raíz
   build: {
-    minify: false, // Desactiva la minificación
+    outDir: 'dist', // Salida del build
   },
-  base: '/', // Asegura que las rutas sean desde la raíz
 });
